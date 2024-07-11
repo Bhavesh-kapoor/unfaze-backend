19e5a78d932e94932adeb7291f7e6ee7cc6a0f15
adarshsrivastav375 <adarshsrivastav.375@gmail.com>
2024-07-11 09:38:06 +0530

    created models of user and session

---
+++ models/sessionsModel.js
@@ -0,0 +1,30 @@
+import mongoose, { Schema } from "mongoose";
+ const sessionSchema = new Schema({
+    user_id:{
+        type: Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+    },
+    therapist_id:{
+        type: Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+    },
+    schedule_time:{
+        type:String,
+        required: true,
+    }, 
+    cost:{
+        type:Number
+    },
+    duration:{
+        type:String,
+        required: true,
+    },
+    status:{
+        type: String,
+        enum: ["upcomming", "completed"],
+        default: "upcomming"
+    } 
+ })
+ export const session = mongoose.model("session", sessionSchema);
---
+++ models/userModel.js
@@ -0,0 +1,97 @@
+import mongoose, { Schema } from "mongoose";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+
+const userSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      lowercase: true,
+      trim: true,
+      unique: true,
+      index: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    mobile: {
+      type: Number,
+      required: true,
+      trim: true,
+      unique: true,
+      index: true,
+    },
+    balance: {
+      type: Number,
+      default: 0,
+    },
+    role: {
+      userType: {
+        type: String,
+        enum: ["user", "admin", "therapist"],
+        default: "student",
+      },
+      age: {
+        type: Number,
+      },
+      isVerified: {
+        type: Boolean,
+        default: false,
+      },
+      profileImage: {
+        type: String,
+        default: "",
+      },
+      refreshToken: {
+        type: String,
+      }
+    }
+  },
+  {
+    timestamps: true,
+  }
+);
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
+userSchema.methods.isPasswordCorrect = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+userSchema.methods.generateAccessToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+      email: this.email,
+      name: this.name,
+    },
+    process.env.ACCESS_TOKEN_KEY,
+    {
+      expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
+    }
+  );
+};
+
+userSchema.methods.generateRefreshToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+    },
+    process.env.REFRESH_TOKEN_KEY,
+    {
+      expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
+    }
+  );
+};
+
+export const User = mongoose.model("User", userSchema);
