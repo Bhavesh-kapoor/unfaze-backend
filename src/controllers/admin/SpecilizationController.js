f36f5d2cc141caa54dda2875bee51a7532a7d387
Bhavesh Kapoor <bhaveshkapoor09@gmail.com>
2024-07-12 14:19:21 +0530

    feat: Specliazation api done

--- src/controllers/admin/SpecilizationController.js
+++ src/controllers/admin/SpecilizationController.js
@@ -1,3 +1,4 @@
+import mongoose from "mongoose";
 import { SpecializationModel } from "../../models/specilaizationModel.js";
 import ApiError from "../../utils/ApiError.js";
 import ApiResponse from "../../utils/ApiResponse.js";
@@ -15,22 +16,48 @@ const createSpecilization = AysncHandler(async (req, res) => {
     const { name } = req.body
     if (!name) res.status(400).json(new ApiError(400, "", "Specilization name is required"));
 
-    const createdExist = SpecializationModel.findOne({ name });
-    if (createdExist) res.status(400).json(new ApiError(400, "", "Already created!"))
+    const createdExist = await SpecializationModel.findOne({ name });
+    if (createdExist) {
 
+        res.status(400).json(new ApiError(400, "", "Already created!"));
+    }
     let isCreated = await SpecializationModel.create({ name });
 
     if (isCreated) {
         res.status(200).json(new ApiResponse(200, "Specilization has been created!"));
     } else {
         res.status(400).json(new ApiError(400, "", "Something went wrong!"));
-
     }
-
-
-
-    console.log(isCreated);
 
 })
 
-export { getAllSpeziliation, createSpecilization };
+
+const updateSpecilization = AysncHandler(async (req, res) => {
+    const { name, id } = req.body;
+    if (!name || !id) {
+        res.status(400).json(new ApiError(400, '', "Name and object id is required!"));
+    }
+    if(!mongoose.Types.ObjectId.isValid(id)){
+        res.status(400).json(new ApiError(400 ,"","Invalid ObjectId"));
+    }
+    let Specilization = await SpecializationModel.findById(id);   
+    Specilization.name =  name; 
+    await Specilization.save();
+
+    res.status(200).json(new ApiResponse(200,Specilization,"Speclization Updated Successfully!"));
+
+})
+
+const deleteSpeclization =  AysncHandler(async(req,res)=>{
+    const { id} =  req.body;
+    if(!id) res.status(400).json(new ApiError(400,"","Please pass object id "));
+    if(!mongoose.Types.ObjectId.isValid(id)) res.staus(400).json(400,"","Invalid Object id");
+ const deletedSpecialization =   await   SpecializationModel.findByIdAndDelete(id);
+ if(!deletedSpecialization ){
+    res.status(400).json(new ApiError(400,"","Speclization Not found!"))
+ }
+    res.status(200).json(new ApiResponse(200,"","Speclization Deleted Successfully!"));
+
+})
+
+export { getAllSpeziliation, createSpecilization, updateSpecilization,deleteSpeclization };
--- src/routes/admin/specilization.route.js
+++ src/routes/admin/specilization.route.js
@@ -1,11 +1,13 @@
 import { Router } from "express";
-import { createSpecilization, getAllSpeziliation } from "../../controllers/admin/SpecilizationController.js";
+import { createSpecilization, getAllSpeziliation,updateSpecilization,deleteSpeclization } from "../../controllers/admin/SpecilizationController.js";
 import verifyJwtToken from "../../middleware/admin/auth.middleware.js";
 let speclizationRoute =  Router();
 
 
 speclizationRoute.get('/all',verifyJwtToken, getAllSpeziliation);
 speclizationRoute.get('/create',verifyJwtToken, createSpecilization);
+speclizationRoute.post('/update',verifyJwtToken, updateSpecilization);
+speclizationRoute.post('/delete',verifyJwtToken, deleteSpeclization);
 
 export default speclizationRoute;
 
