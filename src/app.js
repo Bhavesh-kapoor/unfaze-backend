ff77f84dbbe43a192fc4c25f85df0dbba1c6ac2a
adarshsrivastav375 <adarshsrivastav.375@gmail.com>
2024-07-26 15:16:20 +0530

    refactor: session secret key

--- src/app.js
+++ src/app.js
@@ -20,7 +20,7 @@ app.use(express.urlencoded({ extended: false }));
 // sesssion
 app.use(
   session({
-    secret: "secretkey123455",
+    secret: process.env.SESSION_SECRET_KEY,
     resave: false,
     saveUninitialized: false,
   })
