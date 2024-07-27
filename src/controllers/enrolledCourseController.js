ee670bae7690e7e8984f3f9cf6a00787982495a2
adarshsrivastav375 <adarshsrivastav.375@gmail.com>
2024-07-27 18:33:45 +0530

    feat: course enrollment and list of enrolled course controller

--- src/controllers/enrolledCourseController.js
+++ src/controllers/enrolledCourseController.js
@@ -6,9 +6,9 @@ import asyncHandler from "../utils/asyncHandler.js";
 import { Course } from "../models/courseModel.js";
 
 const validateInput = [
-    check("session_count", "session_count is required").notEmpty(),
-    check("amount", "amount is required").notEmpty(),
-    check("transaction_id", "transaction_id is required").notEmpty(),
+  check("session_count", "session_count is required").notEmpty(),
+  check("amount", "amount is required").notEmpty(),
+  check("transaction_id", "transaction_id is required").notEmpty(),
 ];
 
 const enrollInCourse = asyncHandler(async (req, res) => {
@@ -19,22 +19,35 @@ const enrollInCourse = asyncHandler(async (req, res) => {
       .json(new ApiError(400, "Validation Error", errors.array()));
   }
 
-    const { payment_status, transaction_id, amount } = req.body;
-    const course_id = req.params.course_id; 
-    const user_id = req.user?._id;
-    const course = Course.find({course_id});
-    console.log("course-----",course);
-    const enrolledCourse = new EnrolledCourse({
-        course_id,
-        user_id,
-        payment_status,
-        transaction_id,
-        amount,
-    });
-    await enrolledCourse.save();
+  const { payment_status, transaction_id, amount } = req.body;
+  const course_id = req.params.course_id;
+  const user_id = req.user?._id;
+  const course = Course.find({ course_id });
+  if (!course) {
+    return res.status(404).json(new ApiResponse(404, "", "invalid course"));
+  }
+  const enrolledCourse = new EnrolledCourse({
+    course_id,
+    user_id,
+    payment_status,
+    transaction_id,
+    amount,
+  });
+  await enrolledCourse.save();
 
-
-    res.status(200).json(new ApiResponse(200,enrolledCourse,"Enrolled successfully"));
+  res
+    .status(200)
+    .json(new ApiResponse(200, enrolledCourse, "Enrolled successfully"));
 });
 
-export { enrollInCourse,validateInput };
+const getEnrolledCourseList = asyncHandler(async (req, res) => {
+  const user_id = req.user?._id;
+  const enrolledList = await EnrolledCourse.find({ user_id });
+  if (!enrolledList) {
+    throw new ApiError(404, "You'r not enrolled in any course!");
+  }
+  return res
+    .status(201)
+    .json(new ApiResponse(200, enrolledList, "list fetched successfully"));
+});
+export { enrollInCourse, getEnrolledCourseList, validateInput };
